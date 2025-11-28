a597a132f6def9906a67b29a2f07c832381d48ce
JoelRamos1 <joel.ramos@insbaixcamp.cat>
2025-11-28 13:16:30 +0100

    dashboard changed

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,11 +1,12 @@
 <x-layouts.app :title="__('Dashboard')">
     {{-- <livewire:test> --}}
     <div class="flex h-full w-full flex-1 flex-col gap-4 rounded-xl">
-        <div class="grid auto-rows-min gap-4 grid-cols-2">
+        <div class="grid auto-rows-min gap-4 grid-cols-2 md:grid-cols-3">
             {{-- <div class="relative aspect-video overflow-hidden rounded-xl border border-neutral-200 dark:border-neutral-700">
                 <livewire:shopping-list.components.shopping-lists-info />
             </div> --}}
             <livewire:shopping-list.components.shopping-lists-info />
+
             {{-- <div class="relative aspect-video overflow-hidden rounded-xl border border-neutral-200 dark:border-neutral-700">
                 <x-placeholder-pattern class="absolute inset-0 size-full stroke-gray-900/20 dark:stroke-neutral-100/20" />
             </div> --}}

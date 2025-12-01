4fb780f204a13a829d7d4e8575ed730858c5bf03
JoelRamos1 <joel.ramos@insbaixcamp.cat>
2025-12-01 17:39:13 +0100

    Added Product And Tag Creation

--- app/Livewire/ShoppingList/CategoryEditor.php
+++ app/Livewire/ShoppingList/CategoryEditor.php
@@ -4,6 +4,7 @@ namespace App\Livewire\ShoppingList;
 
 use App\Models\Category;
 use App\Models\Product;
+use App\Models\Tag;
 use Livewire\Attributes\Validate;
 use Livewire\Component;
 
@@ -13,6 +14,8 @@ class CategoryEditor extends Component
 
     #[Validate('required|string|max:255')]
     public $name = '';
+
+    public $tagName = '';
 
     public array $newProductNames = [];
 
@@ -71,6 +74,17 @@ class CategoryEditor extends Component
         $this->authorize('delete', $this->category->shoppingList);
 
         $product->delete();
+
+        $this->category->load('products');
+    }
+
+    public function createTag(int $id)
+    {
+        $product = Product::findOrFail($id);
+
+        $product->tag()->create([
+            'name' => $this->tagName,
+        ]);
 
         $this->category->load('products');
     }

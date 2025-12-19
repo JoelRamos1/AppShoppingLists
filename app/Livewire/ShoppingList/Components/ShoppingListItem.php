f26d5d403fc91eeb2532b362ccbe1a074d113157
JoelRamos1 <joel.ramos@insbaixcamp.cat>
2025-12-19 16:20:01 +0100

    fixed share

--- app/Livewire/ShoppingList/Components/ShoppingListItem.php
+++ app/Livewire/ShoppingList/Components/ShoppingListItem.php
@@ -4,6 +4,7 @@ namespace App\Livewire\ShoppingList\Components;
 
 use App\Models\Category;
 use App\Models\ShoppingList;
+use App\Models\User;
 use Livewire\Component;
 
 class ShoppingListItem extends Component
@@ -13,6 +14,12 @@ class ShoppingListItem extends Component
     public int $totalProducts;
 
     public int $completedProducts;
+
+    public string $newTitle = '';
+
+    public string $userEmail = '';
+
+    public string $role = 'editor';
 
     public function mount(ShoppingList $shoppingList)
     {
@@ -30,6 +37,33 @@ class ShoppingListItem extends Component
         $this->dispatch('list-deleted', id: $this->shoppingList->id);
     }
 
+    public function invite()
+    {
+        $this->validate([
+            'userEmail' => 'required|string|max:255',
+            'role' => 'required',
+        ]);
+
+        $this->authorize('invite', $this->shoppingList);
+
+        $user = User::where('email', $this->userEmail)->first();
+
+        if ($this->shoppingList->members->contains($user)) {
+            return;
+        }
+
+        $this->shoppingList->members()->attach($user->id, ['role' => $this->role]);
+
+        $this->shoppingList->update([
+            'is_shared' => true,
+        ]);
+
+        $this->shoppingList->load('members');
+
+        $this->reset(['userEmail']);
+
+        // return $this->redirectRoute('shopping-lists.index');
+    }
 
     public function render()
     {
